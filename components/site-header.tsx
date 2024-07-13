6f1ccf70c5ac720a267c1c525a0d1d2e61bdd8b5
AayushVinayDev <btech10104.19@bitmesra.ac.in>
2024-07-13 11:14:42 +0530

    added mobile Nav

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -2,10 +2,11 @@ import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
-import { Icons } from "./icons";
-import { MainNav } from "./main-nav";
-import { MobileNav } from "./mobile-nav";
+
 import { ModeToggle } from "./mode-toggle";
+import { MainNav } from "./main-nav";
+import { Icons } from "./icons";
+import { MobileNav } from "./mobile-nav";
 
 export function SiteHeader() {
   return (
