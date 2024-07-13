ff5d9cfee3548c02810d3b94bb0ef9d142bd64ad
AayushVinayDev <btech10104.19@bitmesra.ac.in>
2024-07-13 10:56:07 +0530

    added header and nav

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Quill",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using tRPC, velite, tailwind and shadcn",
+  author: "Aayush Vinay",
+  links: {
+    twitter: "https://twitter.com/AayushVinay",
+    github: "https://github.com/AayushVinayDev",
+    personalSite: "https://AayushVinay.dev",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
